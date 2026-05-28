43450e0ecc743f914631ad98fdfa493305e5eade
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-05-28 16:56:06 +0200

    update-Buttonv1

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,21 @@
 <script>
 import BorderMainSvg from "@/components/svg/BorderMain.svg.vue";
+import ButtonSvg from "@/components/svg/Button.svg.vue";
 export default {
   components: {
     BorderMainSvg,
+    ButtonSvg,
   },
 };
 </script>
 
 <template>
   <BorderMainSvg>
-    <h1 class="text-(--title) text-[clamp(2rem,3vw,3rem)] self-start">
+    <h1 class="text-(--title) text-[clamp(2rem,3vw,2.5rem)] self-start">
       Break the bank
     </h1>
+    <ButtonSvg>
+      <button class="w-full h-full">Commencez</button>
+    </ButtonSvg>
   </BorderMainSvg>
 </template>
