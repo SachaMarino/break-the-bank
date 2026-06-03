dbbd49aa7c4319d6edd7681f80122f7a4ae9d15f
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-06-03 19:10:51 +0200

    update

---
+++ src/components/HeartLife.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      heart: "❤️",
+    };
+  },
+};
+</script>
+<template>
+  <div class="flex">
+    <span v-for="nbr in 3" :key="nbr">{{ heart }}</span>
+  </div>
+</template>
+<style></style>
