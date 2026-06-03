6f742cdca8fb44e294d022e54e435ef298a02109
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-06-03 20:45:32 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script>
+import OrderNumber from "./components/mini-games/OrderNumber.vue";
+import BorderMainSvg from "./components/svg/borderDesign/BorderMain.svg.vue";
 import Home from "./pages/Home.vue";
 export default {
   components: {
     Home,
+    OrderNumber,
+    BorderMainSvg,
   },
 };
 </script>
 
 <template>
   <Home />
+  <BorderMainSvg>
+    <OrderNumber />
+  </BorderMainSvg>
 </template>
---
+++ src/components/mini-games/OrderNumber.vue
@@ -0,0 +1,77 @@
+<script>
+import ButtonSvg from "../svg/borderDesign/Button.svg.vue";
+
+export default {
+  components: {
+    ButtonSvg,
+  },
+  data() {
+    return {
+      checkActive: "bg-(--primary) text-black",
+      checkHover: "hover:bg-(--primary) hover:text-black",
+      maxNumber: 9,
+      currentNumber: 1,
+      shuffleNumbers: [],
+      foundNumbers: [],
+      gameWon: false,
+    };
+  },
+  methods: {
+    startGame() {
+      this.currentNumber = 1;
+      this.foundNumbers = [];
+      this.gameWon = false;
+
+      const numbers = [];
+
+      for (let i = 1; i <= this.maxNumber; i++) {
+        numbers.push(i);
+      }
+      this.shuffleNumbers = numbers.sort(() => Math.random() - 0.5);
+    },
+    checkNumber(number) {
+      if (number === this.currentNumber) {
+        this.foundNumbers.push(number);
+        if (this.currentNumber === this.maxNumber) {
+          this.gameWon = true;
+        } else {
+          this.currentNumber++;
+        }
+      } else {
+        alert("vie perdu, jeux suivant");
+      }
+    },
+  },
+  mounted() {
+    this.startGame();
+  },
+};
+</script>
+<template>
+  <div class="w-full max-w-100">
+    <h1 class="text-(--title) text-3xl">
+      Cliquer les chiffres dans le bon ordre
+    </h1>
+    <div class="flex flex-wrap gap-2 justify-center items-center mt-20">
+      <button
+        v-for="number in shuffleNumbers"
+        :key="number"
+        class="w-full h-auto max-w-25 py-5 border border-(--primary) transition-all duration-100 text-2xl cursor-pointer"
+        :class="[foundNumbers.includes(number) ? checkActive : checkHover]"
+        @click="checkNumber(number)"
+        :disabled="foundNumbers.includes(number)"
+      >
+        {{ foundNumbers.includes(number) ? "✓" : number }}
+      </button>
+
+      <div v-if="gameWon">
+        <h2>Manche suivante</h2>
+        <ButtonSvg>
+          <button @click="startGame" class="py-3">Rejouer</button>
+        </ButtonSvg>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
