102c9407438e9e26ff016ec8cabf2dd70d2b4c2a
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-06-03 20:50:45 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <script>
 import OrderNumber from "./components/mini-games/OrderNumber.vue";
-import BorderMainSvg from "./components/svg/borderDesign/BorderMain.svg.vue";
 import Home from "./pages/Home.vue";
 export default {
   components: {
     Home,
     OrderNumber,
-    BorderMainSvg,
   },
 };
 </script>
 
 <template>
   <Home />
-  <BorderMainSvg>
-    <OrderNumber />
-  </BorderMainSvg>
+  <OrderNumber />
 </template>
--- src/components/mini-games/OrderNumber.vue
+++ src/components/mini-games/OrderNumber.vue
@@ -1,9 +1,11 @@
 <script>
+import BorderMainSvg from "../svg/borderDesign/BorderMain.svg.vue";
 import ButtonSvg from "../svg/borderDesign/Button.svg.vue";
 
 export default {
   components: {
     ButtonSvg,
+    BorderMainSvg,
   },
   data() {
     return {
@@ -48,30 +50,32 @@ export default {
 };
 </script>
 <template>
-  <div class="w-full max-w-100">
-    <h1 class="text-(--title) text-3xl">
-      Cliquer les chiffres dans le bon ordre
-    </h1>
-    <div class="flex flex-wrap gap-2 justify-center items-center mt-20">
-      <button
-        v-for="number in shuffleNumbers"
-        :key="number"
-        class="w-full h-auto max-w-25 py-5 border border-(--primary) transition-all duration-100 text-2xl cursor-pointer"
-        :class="[foundNumbers.includes(number) ? checkActive : checkHover]"
-        @click="checkNumber(number)"
-        :disabled="foundNumbers.includes(number)"
-      >
-        {{ foundNumbers.includes(number) ? "✓" : number }}
-      </button>
+  <BorderMainSvg>
+    <div class="w-full max-w-100">
+      <h1 class="text-(--title) text-3xl">
+        Cliquer les chiffres dans le bon ordre
+      </h1>
+      <div class="flex flex-wrap gap-2 justify-center items-center mt-20">
+        <button
+          v-for="number in shuffleNumbers"
+          :key="number"
+          class="w-full h-auto max-w-25 py-5 border border-(--primary) transition-all duration-100 text-2xl cursor-pointer"
+          :class="[foundNumbers.includes(number) ? checkActive : checkHover]"
+          @click="checkNumber(number)"
+          :disabled="foundNumbers.includes(number)"
+        >
+          {{ foundNumbers.includes(number) ? "✓" : number }}
+        </button>
 
-      <div v-if="gameWon">
-        <h2>Manche suivante</h2>
-        <ButtonSvg>
-          <button @click="startGame" class="py-3">Rejouer</button>
-        </ButtonSvg>
+        <div v-if="gameWon">
+          <h2>Manche suivante</h2>
+          <ButtonSvg>
+            <button @click="startGame" class="py-3">Rejouer</button>
+          </ButtonSvg>
+        </div>
       </div>
     </div>
-  </div>
+  </BorderMainSvg>
 </template>
 
 <style></style>
