c9802032ca00c7f819afca0706a87276459eaea5
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-06-24 13:19:05 +0200

    Update Win.vue

--- src/pages/Win.vue
+++ src/pages/Win.vue
@@ -20,40 +20,50 @@ export default {
 
 <template>
   <BorderMainSvg variant="win" class="background-success flex">
-
     <div class="flex flex-col gap-5 w-full h-full justify-between mb-15">
-
-      <div class="flex w-full  h-[20%] justify-between items-center ">
+      <div class="flex w-full h-[20%] justify-between items-center">
         <div class="flex gap-5 justify-start items-center">
-          <MoneyBagSvg/>
-          <h1 class="text-(--success) text-[clamp(3rem,3vw,2.5rem)] self-center">
+          <MoneyBagSvg />
+          <h1
+            class="text-(--success) text-[clamp(3rem,3vw,2.5rem)] self-center"
+          >
             WINNER
           </h1>
         </div>
-        <PoliceLight variant="win"/>
+        <PoliceLight variant="win" />
       </div>
-      
-      <div class="flex w-full gap-5 h-full justify-evenly  p-5">
-        <div
-          class="flex gap-5 w-[30%] h-full justify-center items-center"
-        >
+
+      <div class="flex w-full gap-5 h-full justify-evenly p-5">
+        <div class="flex gap-5 w-[30%] h-full justify-center items-center">
           <BorderContentSvg variant="win">
-            <h1 class="text-(--success) text-[clamp(3rem,3vw,2.5rem)] w-full">Access Granted</h1>
+            <h1 class="text-(--success) text-4xl w-full m-auto">
+              Access Granted
+            </h1>
           </BorderContentSvg>
         </div>
 
-        <div
-          class="flex gap-5 w-[35%] h-full justify-center items-center"
-        >
-          <TreasureOpenSvg class="relative -top-10"/>
+        <div class="flex gap-5 w-[35%] h-full justify-center items-center">
+          <TreasureOpenSvg class="relative -top-10" />
         </div>
 
-        <div class="flex flex-col w-[20%] gap-10 h-full justify-center items-end">
+        <div
+          class="flex flex-col w-[20%] gap-10 h-full justify-center items-end"
+        >
           <ButtonSvg variant="win" class="max-w-full">
-            <button class="w-full h-full cursor-pointer" @click="$emit('start-game')">Rejouer</button>
+            <button
+              class="w-full h-full cursor-pointer"
+              @click="$emit('start-game')"
+            >
+              Rejouer
+            </button>
           </ButtonSvg>
           <ButtonSvg variant="win" class="max-w-full">
-            <button class="w-full h-full cursor-pointer" @click="$emit('back-home')">Accueil</button>
+            <button
+              class="w-full h-full cursor-pointer"
+              @click="$emit('back-home')"
+            >
+              Accueil
+            </button>
           </ButtonSvg>
         </div>
       </div>
